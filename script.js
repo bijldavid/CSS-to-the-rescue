6f3f738a3cd05350faa32d364b26dfd3b0175a28
David Bijl <davidbijl321@gmail.com>
2025-03-18 18:59:16 +0100

    walking animation

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const ranges = document.querySelectorAll('[type="range"]');
 const updateRange = range => {
     const rangeName = range.name;
     const rangeValue = range.value;
-    const unit = range.dataset.unit;
 
-    document.documentElement.style.setProperty("--" + rangeName, rangeValue + unit);
+    document.documentElement.style.setProperty("--" + rangeName, rangeValue);
 }
 
 ranges.forEach(range => {
